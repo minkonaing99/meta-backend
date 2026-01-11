b99299c95cdec58ea8892139e7ac4e1f9c43b681
minkonaing99 <mkn.minko.naing99@gmail.com>
2026-01-11 21:15:40 +0700

    something

--- day05/recursion.py
+++ day05/recursion.py
@@ -21,7 +21,7 @@ def find_factorial_recursive(n):
     if n == 1:
         return 1
     else:
-        return n* find_factorial_recursive(n-1)
+        return n * find_factorial_recursive(n-1)
     
     
 print(find_factorial_recursive(5))
